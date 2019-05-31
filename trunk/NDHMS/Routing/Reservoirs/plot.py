94aecc81f1e323a53c4fd04bd2e1f0f8f7e9bfd6
John David Mattern <jdmattern@localhost.localdomain>
2019-05-31 16:26:35 -0400

    extra discharge not added in LP overtop

---
+++ trunk/NDHMS/Routing/Reservoirs/plot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import pandas as pd
+#import matplotlib
+#matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("hybrid_logs_3247302.csv", header=0)
+#df = pd.read_csv("hybrid_logs_1312051.csv", header=0)
+#df = pd.read_csv("hybrid_logs_1312051.csv", header=0)
+
+
+print(df.columns)
+
+df[[' Inflow', ' Persisted Outflow', ' Levelpool Outflow', ' Weighted Outflow']].plot()
+
+plt.savefig('fs_plot')
