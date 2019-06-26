f85011349e736b970acf783cd8d98c1ce2fd57a7
John David Mattern <jdmattern@localhost.localdomain>
2019-06-26 13:12:38 -0400

    LP bug fix eval

---
+++ trunk/NDHMS/Routing/Reservoirs/plot_131.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import pandas as pd
+#import matplotlib
+#matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+#df = pd.read_csv("hybrid_logs_120052939.csv", header=0)
+df = pd.read_csv("levelpool_logs_3528295.csv", header=0)
+
+print(df.columns)
+
+#df[['Inflow', 'Water Elevation', 'Outflow']].plot()
+df[['Inflow', 'Outflow']].plot()
+
+plt.savefig('fs_plot_3528295')
